395764432685cd6b71e290e285e3ae8a0a551570
Shafin <58116984+shafiniqbal@users.noreply.github.com>
2020-06-11 06:53:41 +0600

    Add files via upload
    A list of English words consisting only with "ascii_lowercase"

---
+++ Perfect word list.py
@@ -0,0 +1,15 @@
+from string import ascii_uppercase
+k=''
+with open ('Custom Words mid range.csv' , 'w') as wf:
+    for i in ascii_uppercase:
+        readfile = 'words csv\\'+i+'word.csv'
+        with open (readfile , 'r') as rf:
+            for i in rf:
+                if ' ' in i.rstrip() or '-' in i or "'" in i or '"' in i or '#' in i:   
+                    continue
+                elif 6 < len(i) < 14 :             # length of words
+                    if k == i:                     # to remove repeated ones
+                        continue
+                    else:
+                        wf.write(i.rstrip()+'\n')
+                        k=i
